df9116366d7cdb77dfa290ae4a996dac88f7ab7d
Ollivvt <yutingtseng0203@gmail.com>
2025-03-27 12:11:46 -0700

    SettingsPanel with AudioSettings & ResetButton

--- Assets/Scripts/UI/SettingsManager.cs
+++ Assets/Scripts/UI/SettingsManager.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsManager : MonoBehaviour
 {
     [Header("Display Settings")]
     [SerializeField] private Toggle fullscreenToggle;
+
+    [Header("Audio Settings")]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider masterVolumeSlider;
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
+
+    // Default values
+    private const float DEFAULT_MASTER_VOLUME = 1f;
+    private const float DEFAULT_MUSIC_VOLUME = 1f;
+    private const float DEFAULT_SFX_VOLUME = 1f;
 
     private void Start()
     {
@@ -14,11 +26,102 @@ public class SettingsManager : MonoBehaviour
             fullscreenToggle.isOn = Screen.fullScreen;
             fullscreenToggle.onValueChanged.AddListener(SetFullscreen);
         }
+
+        // Initialize audio settings
+        if (masterVolumeSlider != null)
+        {
+            masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume", DEFAULT_MASTER_VOLUME);
+            masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
+        }
+
+        if (musicVolumeSlider != null)
+        {
+            musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", DEFAULT_MUSIC_VOLUME);
+            musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
+        }
+
+        if (sfxVolumeSlider != null)
+        {
+            sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume", DEFAULT_SFX_VOLUME);
+            sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
+        }
+
+        // Apply saved audio settings
+        ApplyAudioSettings();
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
         PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        if (audioMixer != null)
+        {
+            audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+            PlayerPrefs.SetFloat("MasterVolume", volume);
+        }
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        if (audioMixer != null)
+        {
+            audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+            PlayerPrefs.SetFloat("MusicVolume", volume);
+        }
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        if (audioMixer != null)
+        {
+            audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+            PlayerPrefs.SetFloat("SFXVolume", volume);
+        }
+    }
+
+    private void ApplyAudioSettings()
+    {
+        if (audioMixer != null)
+        {
+            SetMasterVolume(PlayerPrefs.GetFloat("MasterVolume", DEFAULT_MASTER_VOLUME));
+            SetMusicVolume(PlayerPrefs.GetFloat("MusicVolume", DEFAULT_MUSIC_VOLUME));
+            SetSFXVolume(PlayerPrefs.GetFloat("SFXVolume", DEFAULT_SFX_VOLUME));
+        }
+    }
+
+    public void ResetToDefaults()
+    {
+        // Reset display settings
+        if (fullscreenToggle != null)
+        {
+            fullscreenToggle.isOn = Screen.fullScreen;
+            PlayerPrefs.SetInt("Fullscreen", Screen.fullScreen ? 1 : 0);
+        }
+
+        // Reset audio settings
+        if (masterVolumeSlider != null)
+        {
+            masterVolumeSlider.value = DEFAULT_MASTER_VOLUME;
+            PlayerPrefs.SetFloat("MasterVolume", DEFAULT_MASTER_VOLUME);
+        }
+
+        if (musicVolumeSlider != null)
+        {
+            musicVolumeSlider.value = DEFAULT_MUSIC_VOLUME;
+            PlayerPrefs.SetFloat("MusicVolume", DEFAULT_MUSIC_VOLUME);
+        }
+
+        if (sfxVolumeSlider != null)
+        {
+            sfxVolumeSlider.value = DEFAULT_SFX_VOLUME;
+            PlayerPrefs.SetFloat("SFXVolume", DEFAULT_SFX_VOLUME);
+        }
+
+        // Apply the reset audio settings
+        ApplyAudioSettings();
     }
 } 
