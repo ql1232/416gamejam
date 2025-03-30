d6f1f06df135834e54f72bc06159d8826e21d51c
ql1232 <hoobooh48@gmail.com>
2025-03-30 05:02:54 -0700

    script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
-
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
     private Vector3 offset;
-    private float maxPlayerY;
+    private float maxPlayerY, prevMaxY;
 
     void Start()
     {
@@ -14,13 +13,25 @@ public class CameraFollow : MonoBehaviour
 
     void Update()
     {
-        if (player.position.y > maxPlayerY)
-        {
+	prevMaxY = maxPlayerY;
+        if(player.position.y > maxPlayerY)
+       {
             maxPlayerY = player.position.y;
         }
 
         Vector3 desiredPosition = player.position + offset;
         desiredPosition.y = maxPlayerY + offset.y;
         transform.position = desiredPosition;
+
+	checkAndPopulate();
     }
+	//normally we do this part in a game manager but this is fine
+   	 void checkAndPopulate(){
+		int level = (int) Mathf.Floor(maxPlayerY/10);
+		if(level > (int) Mathf.Floor(prevMaxY/10)){
+			pop = (int) Random.Range(3,6);
+			
+		}
+	}	
+
 }
